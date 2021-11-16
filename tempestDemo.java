16608dc67300e5092c1013c020bdc9bf09176f96
niaj5 <94445841+niaj5@users.noreply.github.com>
2021-11-17 00:36:38 +0530

    Code Commit without final regex

---
+++ tempestDemo.java
@@ -0,0 +1,45 @@
+import com.sun.jdi.Value;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.security.Key;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class tempestDemo {
+
+    public static void main(String[] args) {
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("src/tempest.txt"));
+
+            String input;
+            StringBuilder sb = new StringBuilder();
+            while ((input = br.readLine()) != null) {
+                sb.append(" ");
+                sb.append(input);
+            }
+            String singleString = sb.toString();
+            Stream<String> inputStream = Stream.of(singleString.split(" "));
+            Map<String, Long> wordMap = inputStream.collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+            wordMap.entrySet().stream()
+                    .sorted((v1,v2)->(v1.getValue()<v2.getValue()?1:(v1.getValue()>v2.getValue()?-1:0))).limit(10)
+                    .forEach(entrySet -> System.out.println(entrySet.getKey() + "(" + entrySet.getValue() +")"));
+
+            br.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+}
+
+//   Stream<String> inputStream = Stream.of(singleString.split(" "));
+//   Map<String, Long> wordMap = inputStream.collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+//   wordMap.entrySet().forEach(entrySet -> System.out.println(entrySet.getKey() + "(" + entrySet.getValue() +")"));
+// System.out.println(singleString);
+           /* Stream.of(singleString.split(" "))
+                    .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                    .entrySet()
+                    .forEach(entrySet -> System.out.println(entrySet.getKey() + "(" + entrySet.getValue() + ")"));*/
